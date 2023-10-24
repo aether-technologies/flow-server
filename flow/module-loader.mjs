e2a6c3fe17c54c47f2acd0bb91f87f7dd7482a75
schurterb <12720208+schurterb@users.noreply.github.com>
2023-10-24 17:40:10 -0400

    Got module-loader at least partially working.

--- flow/module-loader.mjs
+++ flow/module-loader.mjs
@@ -5,24 +5,28 @@ import * as path from 'path';
 export default class ModuleLoaderFlow extends Flow {
     constructor() {
         super("ModuleLoaderFlow");
-        this.root = '../../..';
+        this.root = '../../../../../'; //This path stuff needs polishing and testing
     }
     
     async run(flowMessage) {
+        console.log("FlowMessage :: ",flowMessage);
+        let modulePath = flowMessage.content;
+        let split_path;
         try {
             // the module path should be specified in the flowMessage
-            const modulePath = flowMessage.content;
-            let split_path = modulePath.split(".");
-            split_path.pop();
-            let module_path = split_path.join(".");
+            if(modulePath.includes(".")) {
+                split_path = modulePath.split(".");
+                split_path.pop();
+                modulePath = split_path.join(".");
+            }
             split_path = modulePath.split("/");
             const file_name = split_path.pop();
             
-            console.log("Searching for module: " + module_path);
-            const file_path = findFileInDirectory(this.root, file_name);
-            console.log(" - ",file_path);
+            console.log("Searching for module: " + modulePath);
+            const file_path = await findFileInDirectory('./', file_name);
+            console.log(" - ",this.root + file_path);
 
-            const module = await import(file_path);
+            const module = await import(this.root + file_path);
             
             // you could also access exported members of the module
             // example: const { default: defaultExport, namedExport } = await import(modulePath);
@@ -45,13 +49,14 @@ function fileExists(path) {
     }
 }
 
-function findFileInDirectory(directoryPath, fileName) {
-    const files = fs.readdirSync(directoryPath);
+async function findFileInDirectory(directoryPath, fileName) {
+    console.log("fs :: ",fs);
+    const files = await fs.readdir(directoryPath);
     for (const file of files) {
         const filePath = path.join(directoryPath, file);
-        const fileStat = fs.statSync(filePath);
+        const fileStat = await fs.stat(filePath);
         if (fileStat.isDirectory()) {
-            const foundFilePath = findFileInDirectory(filePath, fileName);
+            const foundFilePath = await findFileInDirectory(filePath, fileName);
             if (foundFilePath) {
                 return foundFilePath;
             }
