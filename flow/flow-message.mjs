c34ab8eca99246af90374d18a67d8c3357664235
schurterb <12720208+schurterb@users.noreply.github.com>
2024-02-05 20:12:20 -0500

    Add metadata parameter to FlowMessage constructor and createResponse method

--- flow/flow-message.mjs
+++ flow/flow-message.mjs
@@ -1,22 +1,21 @@
 import { tools } from './tools.mjs';
 
 export default class FlowMessage {
-    constructor(sender, recipient, content, origin) {
+    constructor(sender, recipient, content, origin, metadata = {}) {
         this.id = tools.uuidv4();
         this.gid = tools.uuidv4();
         this.origin = origin;
         this.sender = sender;
         this.recipient = recipient;
         this.content = content;
+        this.metadata = metadata;
         this.time = new Date().toISOString();
         this.hops = 0;
     }
 
-    createResponse(content) {
-        let response = new FlowMessage(this.recipient, this.sender, content, this.origin);
+    createResponse(content, metadata={}) {
+        let response = new FlowMessage(this.recipient, this.sender, content, this.origin, metadata || {});
         response.gid = this.gid;
         return response;
     }
 }
-
-// export default FlowMessage;
